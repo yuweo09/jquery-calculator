320e25a2c58a395fcd8f19d7f38ebf80745cece9
yuweo09 <Karlwatson09@gmail.com>
2016-08-26 19:43:11 -0700

    No eval

--- js/app.js
+++ js/app.js
@@ -6,23 +6,39 @@ var $screen = $('#screen');
 var $numbers = $('span').not('.operator').on('click',function appendToScreen(event) {
     if(check){  $screen.get(0).innerHTML= "";}
     var $tes = $(event.target);
-    $screen.append($tes.text());
+    //adds a space at the end for the array split method
+    $screen.append($tes.text()+' ');
 });
 //opperand
 var $opperand = $('.operator').on('click',function appendToScreen(event) {
 var $test = $(event.target);
 if($test.get(0).innerHTML === '=' || $test.get(0).innerHTML === 'C'){
-
-}else if ($test.get(0).innerHTML === 'x') {
-    $screen.append('*');
 }else{
-    $screen.append($test.text());
+    $screen.append($test.text()+' ');
 }
+check=false;
 });
 //equals
 var $equals = $('#equals').click(function evaluate(event) {
-$screen.append('='+eval($screen.text()));
+  var str = $screen.text();
+  var arr = str.split(' ');
+  var sum = 0;
+  switch (arr[1]) {
+    case 'x': sum = 1;
+              sum = parseInt(arr[0]) * parseInt(arr[2]);
+              break;
+    case '+':sum = parseInt(arr[0]) + parseInt(arr[2]);
+             break;
+    case '-':sum = parseInt(arr[0]) - parseInt(arr[2]);
+             break;
+    case '\xF7':sum = parseInt(arr[0]) / parseInt(arr[2]);
+             break;
+    default:
+
+  }
+$screen.append('= '+sum+' ');
 check = true;
+sum=0;
 });
 //clear
 var $clear = $('#clear').click(function clear(event) {
